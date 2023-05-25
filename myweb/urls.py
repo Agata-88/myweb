c9509aada4fb8956286b271d47cd871ca5b4ae22
Agata-88 <118485184+Agata-88@users.noreply.github.com>
2023-05-25 22:53:18 +0100

    Update urls.py

--- myweb/urls.py
+++ myweb/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'myweb'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('history/', views.history, name = 'history'),
